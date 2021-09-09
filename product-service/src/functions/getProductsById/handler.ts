95a4084367a442777d8b07b6da4f1a0f2f15d6b3
Nikky <exedystyle@gmail.com>
2021-09-09 18:23:01 +0300

    task-4: stickers from db

--- product-service/src/functions/getProductsById/handler.ts
+++ product-service/src/functions/getProductsById/handler.ts
@@ -1,12 +1,27 @@
 import type { ValidatedEventAPIGatewayProxyEvent } from '@libs/apiGateway';
 import { formatJSONResponse } from '@libs/apiGateway';
 import { middyfy } from '@libs/lambda';
-import { getStickerById } from 'src/utils/utils';
+import { Client } from 'pg';
+import { dbOptions } from 'src/utils/db-options';
+
 
 const getProductsById: ValidatedEventAPIGatewayProxyEvent<typeof Object> = async (event) => {
+  console.log('getProductsById invoked --- ', event);
+  console.log('Received productId --- ', event.pathParameters.productId);
+
+  const client = new Client(dbOptions);
+  await client.connect();
+
   try {
     const { productId } = event.pathParameters;
-    const sticker = await getStickerById(productId);
+    const query = 
+      `select products.*, stocks.count 
+      from products 
+      left join stocks on products.id = stocks.product_id where 
+      id='${productId}'`;
+
+    const { rows: sticker } = await client.query(query);
+    console.log('Received sticker --- ', sticker);
     
     if(!sticker) return formatJSONResponse({msg: 'Sticker not found'}, 404);
 
@@ -15,6 +30,9 @@ const getProductsById: ValidatedEventAPIGatewayProxyEvent<typeof Object> = async
   catch(err) {
     return formatJSONResponse({msg: `something went wrong --- ${err}`}, 500);
   }
+  finally {
+    await client.end();
+  };
 }
 
 export const main = middyfy(getProductsById);
--- product-service/src/functions/getProductsList/handler.ts
+++ product-service/src/functions/getProductsList/handler.ts
@@ -1,11 +1,22 @@
 import type { ValidatedEventAPIGatewayProxyEvent } from '@libs/apiGateway';
 import { formatJSONResponse } from '@libs/apiGateway';
 import { middyfy } from '@libs/lambda';
-import { getAllStickers } from 'src/utils/utils';
+import { Client } from 'pg';
+import { dbOptions } from 'src/utils/db-options';
 
 const getProductsList: ValidatedEventAPIGatewayProxyEvent<typeof Object> = async () => {
+  console.log('getProductsList invoked --- ');
+  const client = new Client(dbOptions);
+  await client.connect();
+
   try {
-    const stickers = await getAllStickers();
+    const query = 
+      `select products.*, stocks.count 
+      from products 
+      left join stocks on products.id = stocks.product_id`;
+    
+    const { rows: stickers } = await client.query(query);
+    console.log('Received stickers --- ', stickers);
 
     if(!stickers) return formatJSONResponse({msg: 'Stickers not found'}, 404);
 
@@ -13,6 +24,9 @@ const getProductsList: ValidatedEventAPIGatewayProxyEvent<typeof Object> = async
   }
   catch(err) {
     return formatJSONResponse({msg: `something went wrong --- ${err}`}, 500);
+  }
+  finally {
+    await client.end();
   }
 }
 
