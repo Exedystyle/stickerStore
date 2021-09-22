8c51d87447d772356320008c24e078ca13eb4905
Nikky <exedystyle@gmail.com>
2021-09-22 23:53:40 +0300

    task-5: initial

---
+++ import-service/serverless.ts
@@ -0,0 +1,28 @@
+import type { AWS } from '@serverless/typescript';
+
+const serverlessConfiguration: AWS = {
+  service: 'import-service-s3',
+  frameworkVersion: '2',
+  custom: {
+    webpack: {
+      webpackConfig: './webpack.config.js',
+      includeModules: true,
+    },
+  },
+  plugins: ['serverless-webpack'],
+  useDotenv: true,
+  provider: {
+    name: 'aws',
+    runtime: 'nodejs14.x',
+    region: 'eu-west-1',
+    stage: 'dev',
+    apiGateway: {
+      minimumCompressionSize: 1024,
+      shouldStartNameWithService: true,
+    },
+    lambdaHashingVersion: '20201221',
+  },
+  functions: {  },
+};
+
+module.exports = serverlessConfiguration;
